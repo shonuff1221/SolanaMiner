ea4c331af7344ec6a36b8be2f3734fca190b15cf
Stephon <shonuff4521@gmail.com>
2021-11-03 14:12:10 -0400

    Update main_eth.js

--- assets/js/main_eth.js
+++ assets/js/main_eth.js
@@ -61,6 +61,15 @@ function controlLoop(){
     refreshData()
     setTimeout(controlLoop,2500)
 }
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    
+    alert("Copied:\n"+ window.location.origin+"?ref="+currentAddr);
+  }
 function controlLoopFaster(){
     liveUpdateEggs()
     // liveUpdatePeers()
